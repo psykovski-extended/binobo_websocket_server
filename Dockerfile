796ac041918de00d8509b8d11e5ff82399b24846
psykovski-extended <dominik.lovetinsky99@gmail.com>
2022-02-28 10:15:41 +0100

    removed database excess, data directly sent back to browser-client

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM python:3.9
 ADD socket_server.py server.py
 RUN pip install websockets
-RUN pip install psycopg2
 EXPOSE 8080
 CMD ["python", "-u", "server.py"]
--- socket_server.py
+++ socket_server.py
@@ -1,57 +1,48 @@
 import asyncio
 import websockets
-import psycopg2
-from ast import literal_eval
-import time
 import socket
 
+receiver_clients = {}
 
-async def store_data(data, token):
-    cursor.execute("select * from data_access_token;")
-    res = cursor.fetchall()
-    token_id = 0
-    for row in res:
-        if row[1] == token:
-            token_id = row[0]
-            break
 
-    if token_id == 0:
-        return False
-
-    time_ = round(time.time() * 1000)
-    query = """INSERT INTO robot_data (
-                if_tip, if_middle, if_base, if_base_rot,
-                mf_tip, mf_middle, mf_base, mf_base_rot,
-                rf_tip, rf_middle, rf_base, rf_base_rot,
-                p_tip, p_middle, p_base, p_base_rot,
-                th_tip, th_base, th_rot_orthogonal, th_rot_palm,
-                wr_lr, wr_bf, data_access_token_id, uploaded_on,
-                expired, sampling_rate
-            ) VALUES
-            """
-    for i in data:
-        query += "(" + str(i[0]) + "," + str(i[1]) + "," + str(i[2]) + "," + str(i[3]) + "," + str(
-            i[4]) + "," + str(i[5]) + "," + str(i[6]) + "," + str(i[7]) + "," + str(
-            i[8]) + "," + str(i[9]) + "," + str(i[10]) + "," + str(i[11]) + "," + str(
-            i[12]) + "," + str(i[13]) + "," + str(i[14]) + "," + str(i[15]) + "," + str(
-            i[16]) + "," + str(i[17]) + "," + str(i[18]) + "," + str(i[19]) + "," + str(
-            i[20]) + "," + str(i[21]) + "," + str(token_id) + "," + str(time_) + "," + "false" + "," + "30),"
-
-    try:
-        cursor.execute(query[:-1] + ";")
-        connection.commit()
-    except Exception:
-        return False
-    return True
+async def add_to_receiver_clients(ws_client, message):
+    token = ws_client.path.split('/')[-1]
+    if message == "close session":
+        for client_index in range(len(receiver_clients[token])):
+            if ws_client == receiver_clients[token][client_index]:
+                del receiver_clients[token][client_index]
+                break
+    else:
+        if token in receiver_clients.keys():
+            receiver_clients[token].append(ws_client)
+        else:
+            receiver_clients[token] = [ws_client]
+        await ws_client.send(str({'STATE': 'OK'}))
+    print(receiver_clients)
 
 
 async def retrieve_data(websocket, path):
-    async for message in websocket:
-        data = literal_eval(message)
-        try:
-            await store_data(data[1], data[0])
-        except Exception as exc:
-            await websocket.send(str({'Error': str(exc)}))
+    if websocket.path != '/':
+        async for message in websocket:
+            await add_to_receiver_clients(websocket, message)
+    else:
+        async for message in websocket:
+            data = eval(message)
+            try:
+                token = data[0]
+                robo_data = data[1]
+                clients_to_delete = []
+                for client_index in range(len(receiver_clients[token])):
+                    try:
+                        await receiver_clients[token][client_index].send(str(robo_data))
+                    except Exception as exc:
+                        print('error occurred: ' + str(exc))
+                        clients_to_delete.append([client_index])
+                clients_to_delete.reverse()
+                for i in clients_to_delete:
+                    del receiver_clients[token][i]
+            except Exception as exc:
+                await websocket.send(str({'Error': str(exc)}))
 
 
 async def main():
@@ -59,25 +50,8 @@ async def main():
     sock.connect(("8.8.8.8", 80))
     ip = sock.getsockname()[0]
     print("Websocket Server running on:\n" + ip + ":8080")
-    async with websockets.serve(retrieve_data, ip, 8080, ping_interval=None):
+    async with websockets.serve(retrieve_data, ip, 8080, ping_interval=None, ping_timeout=None):
         await asyncio.Future()  # run forever
 
-
-connection = None
-# password = input("Your Database Password: ")
-try:
-    connection = psycopg2.connect(host='binobo_database',
-                                  database='binobo_db',
-                                  user='postgres',
-                                  password="password",
-                                  port=5432)
-    time.sleep(1)
-
-    print("Database-Connection status: True")
-
-    cursor = connection.cursor()
-except Exception as e:
-    print(e)
-    print("Database-Connection status: False")
 
 asyncio.run(main())
